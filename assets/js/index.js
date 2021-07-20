fcc3c3dca7ee62c41fa737301a7d24f3d7812a0c
Mihir <mihirsuks@gmail.com>
2021-07-19 19:18:28 -0500

    Add recent youtube videos

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+// Plz don't hack with this key... :(
+var key = 'AIzaSyDutbMPfjzGyVKISkH38y8WGwW3LCYrUKI';
+
+$(document).ready(function() {
+  $.get(
+    "https://www.googleapis.com/youtube/v3/playlistItems", {
+      part: 'snippet',
+      playlistId: 'UUUo0HM2SbVvk-JO17mQQW4A',
+      key: key
+    }, function(data) {
+      data.items.forEach(function(item) {
+        videoId = item.snippet.resourceId.videoId;
+        $('.video-group').append('<div class="video"><iframe width="560" height="315" src=https://www.youtube.com/embed/' + videoId + ' title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>');
+      });
+    }
+  );
+})
